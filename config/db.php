14c491920ccf65223a9cc6e896922dbee41f4859
Elizalde Ulson II <elizaldeulson2451@gmail.com>
2023-04-17 21:49:23 +0800

    second commit

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 	// Create Connection
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	$conn = mysqli_connect("localhost", "root", "root", "elizaldecorp");
 
 	// Check Connection
 	if(mysqli_connect_errno()){
